6e711fada894d40883844eddea1563dce29be9e0
Matt Turnshek <matthewturnshek@gmail.com>
2015-02-27 19:24:21 -0500

    added canvas and resize

--- game.js
+++ game.js
@@ -1,3 +1,15 @@
 $(document).ready(function() {
 
+// Setup
+// Change canvas size
+var canvas = $("#canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+// Objects
+
+// Initialize
+
+// Game Loop
+
 });
