2677fa343ec7eaf149b5f96502fb42301bb15983
Matt Turnshek <matthewturnshek@gmail.com>
2015-02-27 19:51:09 -0500

    attempt to add ship image in js

--- game.js
+++ game.js
@@ -8,8 +8,25 @@ canvas.height = window.innerHeight;
 var context = canvas.getContext("2d");
 
 // Objects
+function Ship(playerController, x, y) {
+    this.playerControl = playerController;
+    this.x = x;
+    this.y = y;
+
+    this.img = document.createElement("img/ourship.png");
+
+    this.update = function() {
+
+    };
+
+    this.draw = function() {
+
+    };
+
+}
 
 // Initialize
+var player = Ship(true, 50, 50);
 
 // Game Loop
 
